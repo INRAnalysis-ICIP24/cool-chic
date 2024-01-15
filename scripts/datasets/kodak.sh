22b815b95c23464798e914f5fb15e613c237d679
aegroto <aegroto@protonmail.com>
2024-01-15 01:47:00 +0100

    dataset scripts

---
+++ scripts/datasets/kodak.sh
@@ -0,0 +1,10 @@
+> schedule.sh
+
+for lambda in "0.0001" "0.0005" "0.004" "0.02"
+do
+    for image_id in {1..24}
+    do        
+        log_file="logs/${config_id}_$i.txt"
+        echo "./samples/run_generic.sh test_images/kodak/${image_id}.png $lambda results/cool-chic-v1/kodak/${lambda}/${image_id}/ > "logs/${lambda}_${image_id}.txt" 2>&1" >> schedule.sh
+    done
+done
