b876e4b7a88f11c53d431ae589df5d033d94b2d8
Lorenzo Catania <lorenzo.catania@phd.unict.it>
2024-01-18 10:13:30 +0000

    improved scripts

--- scripts/datasets/kodak.sh
+++ scripts/datasets/kodak.sh
@@ -1,13 +1,13 @@
 > schedule.sh
 
-for dist_fn in "mse:1.0"
+for dist_fn in "mse:1.0" "l1:1.0" "log_cosh:1.0" "log_cosh:1.0+ssim:0.01" "l1:0.8+ssim:0.2"
 do        
     for lambda in "0.0001" "0.0005" "0.004" "0.02"
     do
         for image_id in {1..24}
         do        
             log_file="logs/${config_id}_$i.txt"
-            echo "./samples/run_generic.sh test_images/kodak/${image_id}.png $lambda results/cool-chic-v2/kodak/${lambda}/${image_id}/ $dist_fn > "logs/${lambda}_${image_id}.txt" 2>&1" >> schedule.sh
+            echo "./samples/run_generic.sh test_images/kodak/${image_id}.png $lambda \"results/cool-chic-v1/kodak/${dist_fn}/${lambda}/${image_id}/\" \"$dist_fn\" > "\"logs/${dist_fn}_${lambda}_${image_id}.txt\"" 2>&1" >> schedule.sh
         done
     done
 done
