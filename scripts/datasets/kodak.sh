8e2663575b00a6b17f8b8fdf02e321a9804751fe
aegroto <aegroto@protonmail.com>
2024-01-31 03:49:16 +0100

    better scripts

--- scripts/datasets/kodak.sh
+++ scripts/datasets/kodak.sh
@@ -1,16 +1,12 @@
-# > schedule.sh
+> schedule.sh
 
-# for dist_fn in "mse:1.0" "l1:1.0" "log_cosh:1.0+ssim:0.01" "l1:0.8+ssim:0.2" 
-for dist_fn in "l1:1.0"
+# for dist_fn in "mse:1.0" "log_cosh:1.0" "l1:1.0" "l1:0.8+ssim:0.2" "log_cosh:1.0+ssim:0.01"
+for dist_fn in "mse:1.0"
 do        
-    # for lambda in "0.0001" "0.0005" "0.004" "0.02"
-    # for lambda in "0.005" "0.007"
-    # for lambda in "0.01" "0.012"
-    # for lambda in "0.08" "0.1" "0.12"
-    for lambda in "0.001" "0.002"     
+    # for lambda in "0.002" "0.005" "0.006" "0.007" "0.008" "0.01" "0.012"
+    for lambda in "0.002" "0.001"
     do
-        for image_id in 11
-        # for image_id in {1..24}
+        for image_id in 11 # {1..24}
         do        
             log_file="logs/${config_id}_$i.txt"
             echo "./samples/run_generic.sh test_images/kodak/${image_id}.png $lambda \"results/cool-chic-v2/kodak/${dist_fn}/${lambda}/${image_id}/\" \"$dist_fn\" " >> schedule.sh
