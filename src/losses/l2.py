c5cb711cfd02550d3cbb2ba56b2e98b7845500b8
aegroto <aegroto@protonmail.com>
2024-06-24 14:47:29 +0200

    README for instructions

--- src/losses/l2.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-class L2Loss(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def __call__(self, original, prediction):
-        diff = original - prediction
-        return diff.square().mean()
